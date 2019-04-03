9ef490418974d6deba33244a39c590c79744fadb
Foysal Mahmud <foysal.km68@gmail.com>
2019-04-03 12:36:29 +0600

    getting all data from https://jsonplaceholder.typicode.com/posts/

---
+++ app/src/main/java/com/example/retrofitpractrce/JsonPlaceHolderApi.java
@@ -0,0 +1,15 @@
+package com.example.retrofitpractrce;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+    @GET("posts/2/comments")
+    Call<List<Comment>> getComments();
+}
