84db428a921f0f695afcf5c00ce33856cb8ef010
Foysal Mahmud <foysal.km68@gmail.com>
2019-04-03 16:19:04 +0600

    Retrofit get, post practice done

--- app/src/main/java/com/example/retrofitpractrce/MainActivity.java
+++ app/src/main/java/com/example/retrofitpractrce/MainActivity.java
@@ -36,9 +36,43 @@ public class MainActivity extends AppCompatActivity {
 
         //getPosts();
         
-        getComments();
+        //getComments();
+
+        createPost();
 
 
+    }
+
+    private void createPost() {
+        Post post = new Post(23, "New Title", "New Text");
+
+        Call<Post> call =  jsonPlaceHolderApi.createPost(post);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if (!response.isSuccessful()){
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+                Post postResponse = response.body();
+                String content = "";
+                content += "Code: "+ response.code() + "\n";
+                content += "ID: "+postResponse.getId()+"\n";
+                content += "User ID: "+postResponse.getUserId()+"\n";
+                content += "Title: "+postResponse.getTitle()+"\n";
+                content += "Text: "+postResponse.getText()+"\n";
+
+                textViewResult.append(content+"\n");
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+
+                textViewResult.setText(t.getMessage());
+
+            }
+        });
     }
 
     private void getComments() {
