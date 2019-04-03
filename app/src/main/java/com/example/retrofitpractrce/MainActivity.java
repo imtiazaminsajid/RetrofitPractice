b6737c12ef3d759a0fc53549253e6765521719d9
Foysal Mahmud <foysal.km68@gmail.com>
2019-04-03 14:19:48 +0600

    Url manipulation @Path @Query by using Retrofit

--- app/src/main/java/com/example/retrofitpractrce/MainActivity.java
+++ app/src/main/java/com/example/retrofitpractrce/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,7 +34,62 @@ public class MainActivity extends AppCompatActivity {
 
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        //getPosts();
+        
+        getComments();
+
+
+    }
+
+    private void getComments() {
+
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments("posts/4/comments");
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+
+                if (!response.isSuccessful()){
+
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+
+                }
+
+                List<Comment> comments = response.body();
+
+                for (Comment comment : comments){
+                    String content = "";
+                    content += "ID: "+comment.getId()+"\n";
+                    content += "Post ID: "+comment.getPostId()+"\n";
+                    content += "Name: "+comment.getName()+"\n";
+                    content += "Email: "+comment.getEmail()+"\n";
+                    content += "Text: "+comment.getText()+"\n";
+
+
+                    textViewResult.append(content+"\n");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+
+                textViewResult.setText(t.getMessage());
+
+            }
+        });
+
+    }
+
+    private void getPosts(){
+
+        Map<String, String> parameters =  new HashMap<>();
+        parameters.put("userId", "2");
+        parameters.put("_sort", "id");
+        parameters.put("_order", "desc");
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -67,5 +124,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 }
